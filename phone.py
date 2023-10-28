749e493f20525bdec191ecd3e54efebab81b5528
saniola <a.kidanov@gmail.com>
2023-10-28 18:53:10 +0300

    add Record methods

--- phone.py
+++ phone.py
@@ -3,6 +3,10 @@ from utils.is_valid_phone import is_valid_phone
 
 class Phone(Field):
     def __init__(self, phone):
-        if not is_valid_phone(phone):
+        if not self.is_valid_phone(phone):
             raise ValueError("Error: The phone number must be 10 digits")
         super().__init__(phone)
+
+    @staticmethod
+    def is_valid_phone(phone):
+        return is_valid_phone(phone)
--- record.py
+++ record.py
@@ -1,4 +1,5 @@
 from name import Name
+from phone import Phone
 
 class Record:
     def __init__(self, name):
@@ -7,3 +8,28 @@ class Record:
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def add_phone(self, phone):
+        if Phone.is_valid_phone(phone):
+            self.phones.append(Phone(phone))
+        else:
+            raise ValueError("Error: The phone number must be 10 digits")
+
+    def remove_phone(self, phone):
+        self.phones = [item for item in self.phones if item.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if Phone.is_valid_phone(new_phone):
+            for item in self.phones:
+                if item.value == old_phone:
+                    item.value = new_phone
+                    return
+            raise ValueError(f"Error: Phone {old_phone} not found in the record.")
+        else:
+            raise ValueError("Error: The phone number must be 10 digits")
+
+    def find_phone(self, phone):
+        for item in self.phones:
+            if item.value == phone:
+                return item.value
+        raise ValueError(f"Error: Phone {phone} not found in the record.")
