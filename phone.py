07241bbd2a74c13e2e70b1da65d746362566fb38
saniola <a.kidanov@gmail.com>
2023-10-28 18:27:55 +0300

    template for task 2

---
+++ phone.py
@@ -0,0 +1,4 @@
+from field import Field
+
+class Phone(Field):
+    pass
---
+++ record.py
@@ -0,0 +1,9 @@
+from name import Name
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
