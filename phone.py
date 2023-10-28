98facb3cfe79a82c6b81d9f676372b83d602f9a1
saniola <a.kidanov@gmail.com>
2023-10-28 18:34:20 +0300

    update phone and name classes

--- phone.py
+++ phone.py
@@ -1,4 +1,8 @@
 from field import Field
+from utils.is_valid_phone import is_valid_phone
 
 class Phone(Field):
-    pass
+    def __init__(self, phone):
+        if not is_valid_phone(phone):
+            raise ValueError("Error: The phone number must be 10 digits")
+        super().__init__(phone)
